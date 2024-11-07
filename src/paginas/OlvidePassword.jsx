d2ba2a7f1aba772a4cdfea1fc03bf63bb6ac0338
Luis Gonzalez <luigis07@hotmail.com>
2024-11-06 18:28:26 -0600

    Update OlvidePassword.jsx

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom'
-import Alerta from '../components/alerta';
+import Alerta from '../components/Alerta';
 import clienteAxios from '../config/axios';
 
 const OlvidePassword = () => {
