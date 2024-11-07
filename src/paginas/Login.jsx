6e490edf46b8f043d8a64f2b0510add879f89f39
Luis Gonzalez <luigis07@hotmail.com>
2024-11-06 18:22:13 -0600

    Update Login.jsx

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Alerta from '../components/alerta';
+import Alerta from "../components/alerta";
 import useAuth from "../hooks/useAuth";
 import clienteAxios from "../config/axios";
 
